985a36e843c38bf07784f1524b81310f427f796c
sivanesh <vssivanesh@gmail.com>
2023-05-10 21:06:55 +0530

    Create RestApi services, and create CRUD application

--- VS CODE/api/api/src/main/java/com/anigna/api/Controller/ApiController.java
+++ VS CODE/api/api/src/main/java/com/anigna/api/Controller/ApiController.java
@@ -1,31 +1,52 @@
 package com.anigna.api.Controller;
-
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.anigna.api.Model.Employee;
-import com.anigna.api.Repository.EmployeeRepository;
 import com.anigna.api.Service.EmployeeService;
 
 @RestController
+@RequestMapping("/api/v1/employee")
 public class ApiController {
     
     @Autowired
     private EmployeeService employeeService;
 
    
-    @GetMapping("/getEmployee/{id}")
-    public Optional<Employee> getEmployee(@PathVariable Integer id){
-        return employeeService.getEmployeeById(id);
+    @GetMapping
+    public List<Employee> getAllEmployee(){
+        return employeeService.retrieve();
     }
 
-    @GetMapping("/getSalary/{id}")
-    public double getSalary(@PathVariable Integer id){
-    return employeeService.calculateSalary(id);
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") int id){
+        Optional<Employee> employee=employeeService.retrieveOne(id);
+        if(employee.isPresent()){
+            return new ResponseEntity<>(employee.get(),HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
+
+    @PostMapping
+    public String saveEmployee(@RequestBody Employee employee){
+        Optional<Employee> _employee=employeeService.create(employee);
+        if(_employee.isPresent()){
+            return "The employee data have been saved sucessfully";
+        }
+        else{
+            return "Employee already exists in record";
+        }
+    }
+
+   
 }
--- VS CODE/api/api/src/main/java/com/anigna/api/Service/EmployeeService.java
+++ VS CODE/api/api/src/main/java/com/anigna/api/Service/EmployeeService.java
@@ -1,10 +1,8 @@
 package com.anigna.api.Service;
-
+import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.anigna.api.Model.Employee;
 import com.anigna.api.Repository.EmployeeRepository;
 
@@ -14,14 +12,21 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    public Optional<Employee> getEmployeeById(int empid){
-        return employeeRepository.findById(empid);
+    public List<Employee> retrieve(){
+        return employeeRepository.findAll();
     }
 
-    public double calculateSalary(int empid){
-        Employee emp=employeeRepository.getById(empid);
-        int experience=emp.getExperience();
-        return experience * 10000;
+    public Optional<Employee> retrieveOne(int empid){
+        return employeeRepository.findById(empid);
+    }
+    
+    public Optional<Employee> create(Employee employee){
+        if(employeeRepository.existsById(employee.getId())){
+            return Optional.empty();
+        }
+        else{
+            return Optional.of(employeeRepository.save(employee));
+        }
     }
     
 }
